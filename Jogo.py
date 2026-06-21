d2b61e35f300c879802a623291689dbaa96b5fe4
Tiago Abreu <tiagoabreu.carv@gmail.com>
2026-06-21 15:32:21 -0300

    Update9 Jogo.py
    1- Posição do sorteio foi alterada.
    2- Adicionado bônus para cada nível escolhido.

--- Jogo.py
+++ Jogo.py
@@ -10,20 +10,23 @@ def game():
         if reais <= 0:
             print("Digite um valor maior que zero!")
             return
-        print("Sorteando multiplicador...")
-        time.sleep(2)
-        multiplicador = random.choices(range(1, 16),weights=[90, 40, 25, 15, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1],k=1)[0]
-        print("Multiplicador Sorteado =",multiplicador,"\n")
         menu = int(input("Escolha uma dificuldade!\n1 para 100 números\n2 para 500 números\n3 para 1000 números\nEscreva aqui->"))
     except ValueError:
         print("Digite apenas números!")
         return
+    print("Sorteando multiplicador...")
+    time.sleep(2)
+    multiplicador = random.choices(range(1, 16),weights=[90, 40, 25, 15, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1],k=1)[0]
+    print("Multiplicador Sorteado =",multiplicador,"\n")
     if menu == 1:
         limite = 100
+        bonus = 1.1
     elif menu == 2:
         limite = 500
+        bonus = 2
     elif menu == 3:
         limite = 1000
+        bonus = 3
     else:
         print("Número inválido")
         return
@@ -49,17 +52,19 @@ def game():
         else:
             print("Você acertou em",tentativas,"tentativas!")
             if tentativas == 1:
-                print("Você ganhou",(reais*multiplicador)*100,"reais!")
+                premio = (reais * multiplicador * bonus) * 100
+                print("Você ganhou R$", round(premio, 2))
                 winsound.Beep(1000, 150)
                 winsound.Beep(1500, 150)
                 winsound.Beep(2000, 150)
                 print("Você é muito sortudo!")
             elif tentativas >=2 and tentativas <=4:
-                print("Você ganhou",(reais*multiplicador)*4,"reais!")
+                premio = (reais * multiplicador * bonus)*4
+                print("Você ganhou R$", round(premio, 2))
                 print("Você é bom!")
-            elif tentativas >=5 and tentativas <=9:
-                print("Você ganhou",reais*multiplicador,"reais!")
-                print("Tá na média, dá pra melhorar")
+            elif tentativas >=5 and tentativas <=7:
+                premio = reais * multiplicador * bonus
+                print("Você ganhou R$", round(premio, 2))
             else:
                 print("Você perdeu a aposta!")
             break
